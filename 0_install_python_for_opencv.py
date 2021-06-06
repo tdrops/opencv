49f640d7956366b8699a069dac5d77c07e007feb
tdrops <69343375+tdrops@users.noreply.github.com>
2021-06-06 13:20:02 +0900

    install

---
+++ 0_install_python_for_opencv.py
@@ -0,0 +1,25 @@
+"""
+opencv
+
+출처
+
+제목: 파이썬으로 만드는 OpenCV 프로젝트
+저자: 이세우
+출판: 프로그래밍인사이트
+"""
+"""
+요약
+
+OpenCV 개발을 위한 프로그램 설치
+"""
+
+"""
+1. Windows 10
+2. Python 3.6.8-64bit 설치(PATH설정)
+3. Opencv 3.4.1.15 설치
+    pip install opencv-contrib-python==3.4.1.15
+    (SIFT, SURF 특허 알고리즘 포함)
+4. Pycharm-community 설치
+   Previously configured interpreter 
+   > System Interpreter 지정
+"""
