d3adf6c4cf24a296dcc1b2db0ced15ba4097ab5f
tdrops <69343375+tdrops@users.noreply.github.com>
2021-06-07 15:45:03 +0900

    186

--- getAffine.py
+++ getAffine.py
@@ -2,9 +2,18 @@ import cv2
 import numpy as np
 import matplotlib.pylab as plt
 
-
 img = cv2.imread(filename="./img/fish.jpg")
+
+h, w = img.shape[:2]  # 높이, 폭 구하기
+
+pts1 = np.float32([[100, 50], [200, 50], [100, 200]])
+pts2 = np.float32([[80, 70], [210, 60], [250, 120]])
+
+matrix = cv2.getAffineTransform(src=pts1, dst=pts2)  # 2점간 대응 행렬 구하기
+result = cv2.warpAffine(src=img, M=matrix, dsize=(w*2, h))  # 행렬 적용
+
 cv2.imshow(winname="original", mat=img)
+cv2.imshow(winname="result", mat=result)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
