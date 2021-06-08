e653af6a2797d66a6a6798ee7c0fa823cb127813
tdrops <69343375+tdrops@users.noreply.github.com>
2021-06-08 20:28:15 +0900

    207

--- workshop_mosaic.py
+++ workshop_mosaic.py
@@ -1,3 +1,19 @@
+"""
+opencv
+
+출처
+
+제목: 파이썬으로 만드는 OpenCV 프로젝트
+저자: 이세우
+출판: 프로그래밍인사이트
+"""
+"""
+요약
+
+마우스로 선택한 영역을 축소와 확대를 하는 방식으로 모자이크 처리한 효과로 변형
+"""
+
+
 import cv2
 import numpy as np
 import matplotlib.pylab as plt
