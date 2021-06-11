e1516b6cdea9de1b552caa57a4a671c9dd4745a5
tdrops <69343375+tdrops@users.noreply.github.com>
2021-06-11 16:47:28 +0900

    231

---
+++ edge_sobel.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+
+img = cv2.imread(filename="./img/sudoku.jpg")
+
+k1 = np.array([[-1,0,1],
+               [-2,0,2],
+               [-1,0,1]])
+k2 = np.array([[-1,-2,-1],
+               [0,0,0],
+               [1,2,1]])
+
+img2 = cv2.filter2D(src=img, ddepth=-1, kernel=k1)
+img3 = cv2.filter2D(src=img, ddepth=-1, kernel=k2)
+
+img4 = cv2.Sobel(src=img, ddepth=-1, dx=1, dy=0, ksize=3)
+img5 = cv2.Sobel(src=img, ddepth=-1, dx=0, dy=1, ksize=3)
+
+hstack1 = np.hstack([img,img2,img3,img2+img3])
+hstack2 = np.hstack([img,img4,img5,img4+img5])
+
+cv2.imshow(winname="sobel", mat=np.vstack([hstack1,hstack2]))
+cv2.waitKey()
+cv2.destroyAllWindows()
