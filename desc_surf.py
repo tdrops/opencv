d6679bcf69ade1073f9d0a0cb10cdf3a7ff5ef7d
tdrops <69343375+tdrops@users.noreply.github.com>
2021-06-18 11:30:30 +0900

    327

---
+++ desc_surf.py
@@ -0,0 +1,26 @@
+"""
+opencv
+
+출처
+
+제목: 파이썬으로 만드는 OpenCV 프로젝트
+저자: 이세우
+출판: 프로그래밍인사이트
+"""
+"""
+요약
+
+surf.detectAndCompute - key points 찾기
+"""
+import cv2
+import numpy as np
+
+
+img = cv2.imread(filename="./img/house.jpg")
+gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+surf = cv2.xfeatures2d.SURF_create(hessianThreshold=1000, nOctaves=3, nOctaveLayers=True, extended=True)
+keypoints, desc = surf.detectAndCompute(gray, None)
+img_draw = cv2.drawKeypoints(image=img, keypoints=keypoints, outImage=None, color=None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv2.imshow(winname="surf", mat=img_draw)
+cv2.waitKey()
+cv2.destroyAllWindows()
