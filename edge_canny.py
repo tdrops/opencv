418d949efd202eaaf5ee7d0db54d568737416d3d
tdrops <69343375+tdrops@users.noreply.github.com>
2021-07-13 12:19:59 +0900

    236

---
+++ edge_canny.py
@@ -0,0 +1,26 @@
+"""
+opencv
+
+출처
+
+제목: 파이썬으로 만드는 OpenCV 프로젝트
+저자: 이세우
+출판: 프로그래밍인사이트
+"""
+"""
+요약
+
+[예제 6-12] 캐니 엣지 검출
+"""
+import cv2
+import numpy as np
+
+img = cv2.imread(filename="../img/sudoku.jpg")
+print(img.shape)
+edges = cv2.Canny(image=img, threshold1=100, threshold2=200)
+print(edges.shape)
+
+cv2.imshow(winname="origin", mat=img)
+cv2.imshow(winname="edges", mat=edges)
+cv2.waitKey()
+cv2.destroyAllWindows()
