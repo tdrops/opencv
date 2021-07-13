7399831668e98f46b7e9bfa49d5d4145589e3f5b
tdrops <69343375+tdrops@users.noreply.github.com>
2021-07-13 11:15:29 +0900

    229

---
+++ edge_prewitt.py
@@ -0,0 +1,28 @@
+"""
+opencv
+
+출처
+
+제목: 파이썬으로 만드는 OpenCV 프로젝트
+저자: 이세우
+출판: 프로그래밍인사이트
+"""
+"""
+요약
+
+[예제 6-8] 프리윗 마스크를 적용한 경계 검출
+"""
+import cv2
+import numpy as np
+
+img = cv2.imread(filename="../img/sudoku.jpg")
+
+gx_k = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
+gy_k = np.array([[-1,-1,-1],[0,0,0],[1,1,1]])
+
+edge_gx = cv2.filter2D(src=img, ddepth=-1, kernel=gx_k)
+edge_gy = cv2.filter2D(src=img, ddepth=-1, kernel=gy_k)
+
+cv2.imshow(winname="ori/gx/gy/gx+gy", mat=np.hstack((img,edge_gx,edge_gy,edge_gx+edge_gy)))
+cv2.waitKey()
+cv2.destroyAllWindows()
