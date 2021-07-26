129e838f4412f2b370be554a1a27548de52288bc
tdrops <69343375+tdrops@users.noreply.github.com>
2021-07-26 09:47:58 +0900

    334

--- match_bf_surf.py
+++ match_bf_surf.py
@@ -1,3 +1,17 @@
+"""
+opencv
+
+출처
+
+제목: 파이썬으로 만드는 OpenCV 프로젝트
+저자: 이세우
+출판: 프로그래밍인사이트
+"""
+"""
+요약
+
+[예제 8-14] BFMatcher 와 SURF 로 매칭
+"""
 import cv2
 import numpy as np
 
@@ -14,7 +28,8 @@ kp2, desc2 = detector.detectAndCompute(gray2, None)
 matcher = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
 
 matches = matcher.match(queryDescriptors=desc1, trainDescriptors=desc2)
-res = cv2.drawMatches(img1=img1, keypoints1=kp1, img2=img2, keypoints2=kp2, matches1to2=matches, outImg=None, flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+res = cv2.drawMatches(img1=img1, keypoints1=kp1, img2=img2, keypoints2=kp2, matches1to2=matches, outImg=None,
+                      flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
 
 cv2.imshow(winname="result", mat=res)
 cv2.waitKey()
