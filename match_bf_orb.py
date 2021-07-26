5e6c9f384f8b5c95d95faadba6fe79d815f6ed0d
tdrops <69343375+tdrops@users.noreply.github.com>
2021-07-26 10:00:16 +0900

    335

---
+++ match_bf_orb.py
@@ -0,0 +1,37 @@
+"""
+opencv
+
+출처
+
+제목: 파이썬으로 만드는 OpenCV 프로젝트
+저자: 이세우
+출판: 프로그래밍인사이트
+"""
+"""
+요약
+
+[예제 8-15] MFMatcher 와 ORB 로 매칭
+"""
+import cv2
+import numpy as np
+
+img1 = cv2.imread(filename="../img/taekwonv1.jpg")
+img2 = cv2.imread(filename="../img/figures.jpg")
+gray1 = cv2.cvtColor(src=img1, code=cv2.COLOR_BGR2GRAY)
+gray2 = cv2.cvtColor(src=img2, code=cv2.COLOR_BGR2GRAY)
+
+detector = cv2.ORB_create()
+
+kp1, desc1 = detector.detectAndCompute(gray1, None)
+kp2, desc2 = detector.detectAndCompute(gray2, None)
+
+matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+matches = matcher.match(queryDescriptors=desc1, trainDescriptors=desc2)
+
+res = cv2.drawMatches(img1=img1, keypoints1=kp1, img2=img2, keypoints2=kp2, matches1to2=matches, outImg=None,
+                      flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+
+cv2.imshow(winname="result", mat=res)
+cv2.waitKey()
+cv2.destroyAllWindows()
