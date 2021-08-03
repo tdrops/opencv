f7d2532220ec10233ea6300f0f564c2cee531c44
tdrops <69343375+tdrops@users.noreply.github.com>
2021-08-03 12:20:56 +0900

    414

--- bow_plane_bike_train.py
+++ bow_plane_bike_train.py
@@ -1,3 +1,17 @@
+"""
+opencv
+
+출처
+
+제목: 파이썬으로 만드는 OpenCV 프로젝트
+저자: 이세우
+출판: 프로그래밍인사이트
+"""
+"""
+요약
+
+[예제 9-13] BOW 와 SVM 으로 비행기와 모터사이클 인식 
+"""
 import cv2
 import numpy as np
 import os
@@ -21,25 +35,25 @@ train_paths = []
 train_labels = []
 
 for idx, category in enumerate(categories):
-    dir_path = base_path+category
-    img_paths = glob.glob(pathname=dir_path+"/*.jpg")
+    dir_path = base_path + category
+    img_paths = glob.glob(pathname=dir_path + "/*.jpg")
     img_len = len(img_paths)
     for i, img_path in enumerate(img_paths):
         train_paths.append(img_path)
         train_labels.append(idx)
         img = cv2.imread(filename=img_path)
         gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
-        kpt,desc = detector.detectAndCompute(gray, None)
+        kpt, desc = detector.detectAndCompute(gray, None)
         bowTrainer.add(descriptors=desc)
-        print('\t%s %d/%d(%.2f%%)' %(category,i+1, img_len, (i+1)/img_len*100), end='\r')
+        print('\t%s %d/%d(%.2f%%)' % (category, i + 1, img_len, (i + 1) / img_len * 100), end='\r')
     print()
 print('Adding descriptor completed...')
 
-print('Starting Dictionary clustering(%d)... It will take several time...'%dictionary_size)
+print('Starting Dictionary clustering(%d)... It will take several time...' % dictionary_size)
 
 dictionary = bowTrainer.cluster()
 np.save(dict_file, dictionary)
-print('Dictionary Clustering completed...dictionary shape:',dictionary.shape)
+print('Dictionary Clustering completed...dictionary shape:', dictionary.shape)
 
 bowExtractor.setVocabulary(vocabulary=dictionary)
 train_desc = []
@@ -48,7 +62,7 @@ for i, path in enumerate(train_paths):
     gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
     hist = bowExtractor.compute(image=gray, keypoints=detector.detect(gray))
     train_desc.extend(hist)
-    print('Compute histogram training set...(%.2f%%)' %((i+1)/len(train_paths)*100),end='\r')
+    print('Compute histogram training set...(%.2f%%)' % ((i + 1) / len(train_paths) * 100), end='\r')
 
 print("\nsvm items", len(train_desc), len(train_desc[0]))
 
@@ -57,14 +71,14 @@ svm = cv2.ml.SVM_create()
 svm.trainAuto(np.array(train_desc), cv2.ml.ROW_SAMPLE, np.array(train_labels))
 svm.save(svm_model_file)
 print('svm training completed.')
-print('Training Elapsed: %s'%time.strftime('%H:%M:%S', time.gmtime(time.time()-startT)))
+print('Training Elapsed: %s' % time.strftime('%H:%M:%S', time.gmtime(time.time() - startT)))
 
 print("Accuracy(Self)")
 
 for label, dir_name in enumerate(categories):
     labels = []
     results = []
-    img_paths = glob.glob(pathname=base_path+"/"+dir_name+"/*.*")
+    img_paths = glob.glob(pathname=base_path + "/" + dir_name + "/*.*")
     for img_path in img_paths:
         labels.append(label)
         img = cv2.imread(filename=img_path)
@@ -78,17 +92,4 @@ for label, dir_name in enumerate(categories):
     results = np.array(results)
     err = (labels != results)
     err_mean = err.mean()
-    print('\t%s: %.2f %%' % (dir_name, (1 - err_mean)*100))
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print('\t%s: %.2f %%' % (dir_name, (1 - err_mean) * 100))
